010ba4bba029c20f23600f716dbda2f8765905bc
jacky <chenjacky@gmail.com>
2018-01-16 11:53:14 +0800

    index

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
-<<<<<<< HEAD
 import Promo from '@/views/Promo'
 import Cqssc from '@/views/lottery/Cqssc'
-=======
 import Promo from '@/views/promo/Promo'
->>>>>>> cfcadd8fb3246699366a0b74399ad3f27774b8ce
-
 Vue.use(Router)
 
 export default new Router({
